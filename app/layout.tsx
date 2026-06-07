de2841321c82e5f5c80314954e8700cdbaf2a104
Furqan Ghias <furqanghias0@gmail.com>
2026-06-07 20:26:52 +0500

    Added speed analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import './globals.css';
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
 import SmoothCursor from '../components/SmoothCursor';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
@@ -40,6 +41,7 @@ export default function RootLayout({
         <Footer />
         <SmoothCursor />
          <Analytics />
+          <SpeedInsights />
       </body>
     </html>
   );
